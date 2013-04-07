4c06956e49dd2301aadcf23657270a75e7364c56
Josh Spector <panduh@Joshs-MacBook-Pro.local>
2013-04-07 02:26:00 -0400

    added goodsync.app and LoL

--- modules/people/manifests/panderp/applications.pp
+++ modules/people/manifests/panderp/applications.pp
@@ -18,5 +18,16 @@ class people::panderp::applications (
   include handbrake
   include caffeine
 
+package { 'GoodSync':
+	source		=> 'http://www.goodsync.com/download/goodsync-mac.dmg',
+	provider	=> appdmg,
+	}
+
+package { 'League of Legends':
+	source		=> 'http://l3cdn.riotgames.com/Installer/NA_Mac_Installer/League%20of%20Legends%20NA.dmg',
+	provider	=> 'appdmg',
+	}
+
+
 }
 
